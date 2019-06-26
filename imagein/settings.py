07630066b8ce5407e4a557c00d84a861b4d81371
Aditya Chandla <36351155+adityachandla@users.noreply.github.com>
2019-06-26 12:32:17 +0530

    Update settings.py

--- imagein/settings.py
+++ imagein/settings.py
@@ -122,7 +122,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST_USER = 'adityachandla@gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_PASSWORD = 'robinhood1770'
+EMAIL_HOST_PASSWORD = ##
 
 
 MEDIA_URL = '/media/'
